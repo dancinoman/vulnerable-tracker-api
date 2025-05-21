027de8a93577745ecda94810a10469ea5819e454
Hugo Guay <dancinoman@gmail.com>
2025-05-21 17:45:05 -0400

    osv working

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import config
 from fastapi import FastAPI
+
 # Project imports
 from .core import parser as psr
 from .core import osv
@@ -12,14 +13,3 @@ app = FastAPI(
 )
 
 app.include_router(router)
-
-if __name__ == "__main__":
-    # Open the sample_requirements.txt file and read its content
-    with open(config.TEST_DATA_PATH  + "/sample_requirements.txt") as f:
-        requirements = f.read()
-
-
-    valid, ignored = psr.parse_requirements(requirements)
-
-    print("Valid dependencies:", valid)
-    print("Ignored dependencies:", ignored)
